5d39b4f179da0b601e87bd51a06751ce9c6b7fd7
robinson <robinjosellove@gmail.com>
2021-09-22 21:11:34 -0500

    rick and morty

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rick and Morty
+![Rick and Morty](https://psycatgames.com/magazine/party-games/rick-and-morty/feature-image_hu925c11e614a05597a70d8de781816325_3092312_1600x1200_fill_q100_box_smart1.jpg)
--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import { useEffect, useState } from 'react';
-import Characters from './components/Characters';
-import Header from './components/Header';
-import './assets/styles/App.scss';
-import './assets/styles/Header.css';
+import { useEffect, useState } from "react";
+import Characters from "./components/Characters";
+import Header from "./components/Header";
+import "./assets/styles/App.scss";
+import "./assets/styles/Header.css";
 
 function App() {
-    const [character, setCharacter] = useState([]);
-    const [search, setSearch] = useState('');
+  const [character, setCharacter] = useState([]);
+  const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character')
-        .then(response => response.json())
-        .then(data => setCharacter(data.results))
-    }, [])
-    return(
-        <div className="app">
-            <Header
-            title = "Rick and Morty"
-            />
-            <input type="text" placeholder="Search of a character" className="search" 
-            onChange={e => setSearch(e.target.value)}
-            />
-            <Characters character={character} search={search}/>
-        </div>
-    )
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((data) => setCharacter(data.results));
+  }, []);
+  return (
+    <div className="app">
+      <Header title="Rick and Morty" />
+      <div className="background">
+        <input
+          type="text"
+          placeholder="Search of a character"
+          className="search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Characters character={character} search={search} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
